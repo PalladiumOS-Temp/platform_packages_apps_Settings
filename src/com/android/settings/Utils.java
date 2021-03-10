56707631ed81d3029082182475916448f76e83e4
Lokesh773 <lokeshkasamneni773@gmail.com>
2021-03-10 20:22:22 +0530

    [SQUASH]settings:revert face lock old implementation
    Signed-off-by: Lokesh773 <lokeshkasamneni773@gmail.com>

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -147,8 +147,6 @@ public final class Utils extends com.android.settingslib.Utils {
      */
     public static final String PROPERTY_PERMISSIONS_HUB_ENABLED = "permissions_hub_enabled";
 
-    public static final String PIN_PASSWORD_LENGTH = "lockscreen.pin_password_length";
-
     /**
      * Finds a matching activity for a preference's intent. If a matching
      * activity is not found, it will remove the preference.
@@ -1154,42 +1152,6 @@ public final class Utils extends com.android.settingslib.Utils {
         drawable.draw(canvas);
         return roundedBitmap;
     }
-
-    public static int getPINPasswordLength(LockPatternUtils lockPatternUtils, int userId) {
-        int pinLength = 0;
-        try {
-            pinLength = (int) lockPatternUtils.getLockSettings().getLong(PIN_PASSWORD_LENGTH, 0, userId);
-        } catch (Exception e) {
-            Log.d("getPINPasswordLength", "getLong error: " + e.getMessage());
-        }
-        if (pinLength >= 4) {
-            return pinLength;
-        }
-        return 0;
-    }
-
-    public static void savePINPasswordLength(LockPatternUtils lockPatternUtils, int length, int userId) {
-        try {
-            lockPatternUtils.getLockSettings().setLong(PIN_PASSWORD_LENGTH, (long) length, userId);
-        } catch (Exception e) {
-            Log.d("savePINPasswordLength", "saveLong error: " + e.getMessage());
-        }
-    }
-
-    public static boolean isFaceDisabledByAdmin(Context context) {
-        DevicePolicyManager devicePolicyManager = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-        try {
-            if (devicePolicyManager.getPasswordQuality(null) > DevicePolicyManager.PASSWORD_QUALITY_MANAGED) {
-                return true;
-            }
-        } catch (SecurityException e) {
-            Log.e("Settings", "isFaceDisabledByAdmin error:", e);
-        }
-        if ((devicePolicyManager.getKeyguardDisabledFeatures(null) & DevicePolicyManager.KEYGUARD_DISABLE_FACE) != 0) {
-            return true;
-        }
-        return false;
-      }
 
     /**
      * Returns {@code true} if needed to disable media output, otherwise returns {@code false}.
